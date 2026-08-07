4b49d1377b39f938cc01d7ff2ea419d45b2a31fc
Igor Grieder Melo <igorgrieder@gmail.com>
2026-08-06 21:16:48 -0300

    add method

--- handlers.go
+++ handlers.go
@@ -1,10 +1,25 @@
 package main
 
-import maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+import (
+	"encoding/json"
+	"sync"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
 
 func handlers(node *maelstrom.Node) {
+	store := safeLogger{keyStores: make(map[string]keyStores, 0), loggerLock: sync.RWMutex{}}
 	node.Handle("send", func(msg maelstrom.Message) error {
-		offset := 1000
+		var body sendRequest
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		offset, err := store.Add(body.Key, body.Msg)
+		if err != nil {
+			return err
+		}
+
 		response := &sendResponse{MessageType: "send_ok", Offset: offset}
 		return node.Reply(msg, response)
 
--- store.go
+++ store.go
@@ -1,6 +1,10 @@
 package main
 
-import "sync"
+import (
+	"fmt"
+	"strconv"
+	"sync"
+)
 
 type safeLogger struct {
 	loggerLock sync.RWMutex
@@ -8,7 +12,38 @@ type safeLogger struct {
 }
 
 type keyStores struct {
-	keyLock  sync.RWMutex
-	messages map[string]int
+	messages []message
 	offsets  int
+}
+
+type message struct {
+	messageSent int
+	offset      int
+}
+
+func (sL *safeLogger) Add(key string, value int) (int, error) {
+	sL.loggerLock.Lock()
+	defer sL.loggerLock.Unlock()
+
+	keyStore, ok := sL.keyStores[key]
+	slicedKey := key[1:]
+
+	offsettBase, err := strconv.Atoi(slicedKey)
+	if err != nil {
+		fmt.Println("Conversion error:", err)
+		return 0, err
+	}
+
+	// If map is empty
+	if !ok {
+		newStore := keyStores{messages: []message{message{offset: offsettBase * 1000, messageSent: value}}, offsets: offsettBase * 1000}
+		sL.keyStores[key] = newStore
+		return 0, nil
+	}
+
+	offset := keyStore.offsets + 1
+	keyStore.messages = append(keyStore.messages, message{offset: offset + 1, messageSent: value})
+	keyStore.offsets = offset
+
+	return offset, nil
 }
